3b8c81e737262ad62580252480664e025f553a2e
“xixn2” <ssbkh0802@naver.com>
2024-10-15 00:19:26 +0900

    ✨ :: [#4] Add Customization Color, Font using code edit

---
+++ Projects/App/Sources/DesignSystem/DesignModifire.swift
@@ -0,0 +1,25 @@
+//
+//  DesignModifire.swift
+//  DaeSup
+//
+//  Created by 서지완 on 10/15/24.
+//  Copyright © 2024 Mindway-DaeSup. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+typealias Font = DaeSupFontFamily.Pretendard
+typealias Color = DaeSupAsset.Color
+
+extension View {
+    func daesupFont(_ font: DaeSupFontConvertible, size: CGFloat) -> some View {
+        self.font(font.swiftUIFont(size: size))
+    }
+    //.daesupFont(Font.폰트종류, size: 사이즈)
+    
+    func daesupColor(_ color: DaeSupColors) -> some View {
+            self.foregroundColor(color.swiftUIColor)
+    }
+    //.daesupColor(Color.색상종류)
+}
