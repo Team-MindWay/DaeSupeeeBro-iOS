d9bfa1563d836ac42f6ad9fc80715868fe330052
“xixn2” <ssbkh0802@naver.com>
2024-10-15 00:16:06 +0900

    🔥 :: [#4] Delete SceneDelegate, AppDelegate / Add DaeSupApp File

---
+++ Projects/App/Sources/Application/DaeSupApp.swift
@@ -0,0 +1,19 @@
+//
+//  DaeSupApp.swift
+//  DaeSup
+//
+//  Created by 서지완 on 10/15/24.
+//  Copyright © 2024 Mindway-DaeSup. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ExpoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //
+        }
+    }
+}
+
