a49948f8f0a056c24f7bce343419bdf5d56853eb
hotcat0421 <s24044@gsm.hs.kr>
2024-10-21 11:46:06 +0900

    init: 기본 세팅

---
+++ Projects/App/Sources/Feature/MainFeature/Sources/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  MainView.swift
+//  DaeSup
+//
+//  Created by 박정우 on 10/17/24.
+//  Copyright © 2024 Mindway-DaeSup. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
